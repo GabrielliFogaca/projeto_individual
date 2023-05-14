e0a0b59528d0c13247d0ffeca141dcf6cbe8884a
GabrielliFogaca <gabigallionef@gmail.com>
2023-05-14 16:30:05 -0300

    Integrando a API ao site

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -63,23 +63,23 @@ function entrar(req, res) {
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
-    var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var origem = req.body.origemServer
     var casa = req.body.casaServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
+    } else if (origem == undefined) {
+        res.status(400).send("Sua origem está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
     } else if (casa == undefined) {
-        res.status(400).send("Sua senha está undefined!");
+        res.status(400).send("Sua casa está undefined!");
     } else {
         
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, casa)
+        usuarioModel.cadastrar(nome, origem, senha, casa)
             .then(
                 function (resultado) {
                     res.json(resultado);
